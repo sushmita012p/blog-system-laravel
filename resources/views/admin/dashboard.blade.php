fcb68cab2baee56d3390def1bd4cc1c412be0754
Sushmita Poudel <sushmitapoudel071@gmail.com>
2023-05-18 17:42:14 +0545

    Implement logout functionality

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,7 +15,13 @@
         <div class="card">
             <div class="card-header">
                 <h4 class="text-center">Admin Dashboard</h4>
-                <a href="/" class="float-right">Logout</a>
+                <a href="{{ route('logout') }}" class="btn btn-danger btn-sm float-right"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
             </div>
             <div class="card-body">
                 <a href="{{ url('admin/category') }}" class="btn btn-primary btn-sm">View Category</a>
